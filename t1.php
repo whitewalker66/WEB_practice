9e54b7c61cafc381350bad1ee7c5daa8f2bea4c9
whitewalker66 <64178685+whitewalker66@users.noreply.github.com>
2023-03-07 09:36:49 +0530

    Add files via upload

---
+++ t1.php
@@ -0,0 +1,24 @@
+
+<html>
+    <body>
+        </body>
+       <!-- <?php
+        // $a="this is outside function";
+        // function mytest()
+        // {
+        //     global $x;
+        //     $x="this is inside function";
+        //     echo $x;
+        //     echo $GLOBALS['a'];
+        // }
+        // mytest();
+        // echo"value of x inside function=" ,$x;
+//------------------------------------------------------------------------
+?>  -->
+//      FORM VALIDATION
+<BR>
+YOUR NAME:<?php echo $_POST["name"];?><BR>
+ADDRESS:<?php echo $_POST["address"];?><BR>
+PHONE NUMBER:<?php echo $_POST["phone"];?>
+        </body>
+</html>
